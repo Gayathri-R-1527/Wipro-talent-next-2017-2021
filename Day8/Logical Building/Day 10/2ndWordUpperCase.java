e383f8b36d9d415f6fafa5b1658faec860d4d6fc
Gayathri1527 <64785679+Gayathri-R-1527@users.noreply.github.com>
2020-06-06 10:44:29 +0530

    Create 2ndWordUpperCase.java

---
+++ Day8/Logical Building/Day 10/2ndWordUpperCase.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class UserMainCode
+{
+public int String getSecondWordInUppercase(String input1) {
+		if (input1.equals("")) 
+    return "LESS";
+		
+		StringBuffer sb = new StringBuffer(input1.trim());
+		
+		try {
+			int si = sb.indexOf(" ");
+			if (si== -1)
+      return "LESS";
+			
+			try {
+				int ei = sb.indexOf(" ", si+1) + 1;
+				return sb.substring(si+1, endIndex).toUpperCase().trim();
+			} catch (StringIndexOutOfBoundsException e) {
+				
+				return sb.substring(si+1).toUpperCase().trim();
+			}
+		} catch (StringIndexOutOfBoundsException e) {
+			
+			return "LESS";
+		}
+	}
