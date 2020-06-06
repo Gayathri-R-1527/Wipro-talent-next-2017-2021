a50c5894984d408656e715a4a0be5d560b2ab62d
Gayathri1527 <64785679+Gayathri-R-1527@users.noreply.github.com>
2020-06-06 13:13:11 +0530

    Create additionof2strings.java

---
+++ Day9/LB-Milestone 2/String based part-1/additionof2strings.java
@@ -0,0 +1,18 @@
+import.java.io.*;
+import java.io.util;
+class UserMainCode{
+public String addNumberstrings(String input1,String input2){
+int sout;
+		
+		int a = Integer.parseInt(input1);
+		int b = Integer.parseInt(input2);
+		sout=a+b;
+		String s= Integer.toString(sout);
+		return s;
+		
+		
+		
+	}
+	
+	
+}
